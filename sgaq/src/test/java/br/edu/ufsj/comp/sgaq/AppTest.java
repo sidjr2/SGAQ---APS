68d8955e0d6d3f891fafaf62a29a7c52c335e485
sid ufsj <sidney.fejr@aluno.ufsj.edu.br>
2023-09-14 09:45:13 -0300

    CRIANDO PARA APS

---
+++ sgaq/src/test/java/br/edu/ufsj/comp/sgaq/AppTest.java
@@ -0,0 +1,20 @@
+package br.edu.ufsj.comp.sgaq;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
